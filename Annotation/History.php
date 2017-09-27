fe6019fcbfa3e16338966136d52c994fe50b646e
jérémy pasini <jeremy.pasini1@gmail.com>
2017-09-27 10:52:01 +0200

    [feature subscriber] create automaticaly history of enties have annoted with History annotation

---
+++ Annotation/History.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Resomedia\EntityHistoryBundle\Annotation;
+use Doctrine\Common\Annotations\Annotation\Target;
+
+/**
+ * The History class handles the @History annotation.
+ * @Annotation
+ * @Target("CLASS")
+ */
+class History {
+    //Just an placeholder
+}
